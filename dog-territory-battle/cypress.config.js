2833f52867276ec6110d38bf6d9a12873ae6a844
Vegitable-price <fumitatsu2748@gmail.com>
2024-10-08 10:33:21 +0900

    WIP

--- dog-territory-battle/cypress.config.js
+++ dog-territory-battle/cypress.config.js
@@ -1,10 +1,14 @@
-const { defineConfig } = require("cypress");
+// cypress.config.js
+const { defineConfig } = require('cypress');
+const codeCoverageTask = require('@cypress/code-coverage/task');
 
 module.exports = defineConfig({
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+  e2e: {
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config);
+      return config;
     },
+    // その他の設定
   },
+  // その他の設定
 });
